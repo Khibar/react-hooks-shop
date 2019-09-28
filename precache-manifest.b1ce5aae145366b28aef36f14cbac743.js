a049e55171ee650c740b7a26b3e13ffcb7939112
Khibar Rassul <kr@Khibars-iMac.local>
2019-09-28 20:41:06 +0700

    build files

---
+++ precache-manifest.b1ce5aae145366b28aef36f14cbac743.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1bc1facf80dec87efa3c78009a8cd332",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1fd32218933b43e1c749",
+    "url": "/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "40b169f46b78367c4620",
+    "url": "/static/css/main.ff90eeeb.chunk.css"
+  },
+  {
+    "revision": "1fd32218933b43e1c749",
+    "url": "/static/js/2.4a22d938.chunk.js"
+  },
+  {
+    "revision": "40b169f46b78367c4620",
+    "url": "/static/js/main.e7083f35.chunk.js"
+  },
+  {
+    "revision": "8e47035760337b38a943",
+    "url": "/static/js/runtime-main.b859ef43.js"
+  },
+  {
+    "revision": "24780abf3bca3d52eaa3629a8cf7f59e",
+    "url": "/static/media/bg-autumn.24780abf.jpg"
+  },
+  {
+    "revision": "a77882f1b8860a94ddf9595ba5d5ac09",
+    "url": "/static/media/bg-landing.a77882f1.jpg"
+  },
+  {
+    "revision": "0b01c8410fdabe138fac3bc4103ac69c",
+    "url": "/static/media/bg-shop.0b01c841.jpg"
+  },
+  {
+    "revision": "8bbabc23017e3987e3d3051c7560be50",
+    "url": "/static/media/bg-webshop.8bbabc23.jpg"
+  }
+]);
